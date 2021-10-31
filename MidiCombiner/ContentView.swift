75ca79b715e877eb4fdf015173549085deb3062a
joshuadutton <joshuadutton@gmail.com>
2021-10-31 12:25:40 -0600

    Initial Commit

---
+++ MidiCombiner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MidiCombiner
+//
+//  Created by Joshua Dutton on 10/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MidiCombiner/MidiCombinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidiCombinerApp.swift
+//  MidiCombiner
+//
+//  Created by Joshua Dutton on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct MidiCombinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
